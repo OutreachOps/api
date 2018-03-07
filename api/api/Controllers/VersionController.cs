6209a1ac9f12312300b39c011282e4ffd454ca30
Adrian Russell <adrian.f.russell@gmail.com>
2018-03-07 22:05:30 +1100

    Add initial project

---
+++ api/api/Controllers/VersionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+
+namespace api.Controllers
+{
+    public class Version
+    {
+        public int Software { get; set; }
+    }
+
+    public class VersionController : ApiController
+    {
+
+        public IHttpActionResult GetVersion()
+        {
+            var version = new Version(){Software = 1};
+
+            return Ok(version);
+        }
+    }
+}
