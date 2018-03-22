53e26d6fc90f21959afb73b4381905028da6918f
AdrianRussell <adrian.f.russell@gmail.com>
2018-03-22 17:36:25 +1100

    add password hash library.

---
+++ src/api.test/Domain/Security/PasswordHashFixture.cs
@@ -0,0 +1,18 @@
+﻿using OutreachOperations.Api.Domain.Security;
+using Xunit;
+
+namespace OutreachOperations.Api.Test.Domain.Security
+{
+    public class PasswordHashFixture
+    {
+        [Fact]
+        public void CanHashAndVerifyPassword()
+        {
+            var passwordHashUtility = new BCryptPasswordHash();
+
+            var hashed = passwordHashUtility.HashPassword("password");
+
+            Assert.True(passwordHashUtility.VerifyPassword("password", hashed));
+        }
+    }
+}
---
+++ src/api/Domain/Security/PasswordHash.cs
@@ -0,0 +1,21 @@
+﻿namespace OutreachOperations.Api.Domain.Security
+{
+    interface PasswordHash
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string password, string passwordHash);
+    }
+
+    public class BCryptPasswordHash : PasswordHash
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
