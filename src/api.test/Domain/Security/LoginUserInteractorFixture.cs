6ba3ef36e246d3299f2d0a06d7d1afd325f6d54d
AdrianRussell <adrian.f.russell@gmail.com>
2018-03-23 19:04:20 +1100

    Add login controller

---
+++ src/api.test/Domain/Security/LoginUserInteractorFixture.cs
@@ -0,0 +1,62 @@
+﻿using Moq;
+using OutreachOperations.Api.Domain.Security;
+using Xunit;
+
+namespace OutreachOperations.Api.Test.Domain.Security
+{
+    public class LoginUserInteractorFixture
+    {
+        private readonly string _emailAddress = "email@email.com";
+
+        [Fact]
+        public void Execute_PasswordNotVerified_UserNotLoggedIn()
+        {
+            BCryptPasswordHash pwh = new BCryptPasswordHash();
+            var hashedPassword = pwh.HashPassword("AReallyStringPassword");
+
+            var emailQuery = new Mock<FindUserQueryByEmail>();
+            emailQuery.Setup(x => x.Execute(_emailAddress)).Returns(new User
+                { EmailAddress = _emailAddress,PasswordHash = hashedPassword });
+
+            var interactor = new LoginUserInteractor(emailQuery.Object,pwh);
+
+            var result = interactor.Execute(new LoginRequest{EmailAddress = _emailAddress,Password = "The incorrect password"});
+
+            Assert.Equal("User Not Logged In",result.ResponseMessage);
+        }
+
+        [Fact]
+        public void Execute_EmailAddressNotFound_UserNotLoggedIn()
+        {
+            BCryptPasswordHash pwh = new BCryptPasswordHash();
+
+            var emailQuery = new Mock<FindUserQueryByEmail>();
+            emailQuery.Setup(x => x.Execute(_emailAddress)).Returns((User)null);
+
+            var interactor = new LoginUserInteractor(emailQuery.Object, pwh);
+
+            var result = interactor.Execute(new LoginRequest { EmailAddress = _emailAddress, Password = "AReallyStringPassword" });
+
+            Assert.Equal("User Not Logged In", result.ResponseMessage);
+        }
+
+        [Fact]
+        public void Execute_EmailFoundAndPasswordVerified_UserLoggedIn()
+        {
+            BCryptPasswordHash pwh = new BCryptPasswordHash();
+            var hashedPassword = pwh.HashPassword("AReallyStringPassword");
+
+            var emailQuery = new Mock<FindUserQueryByEmail>();
+            emailQuery.Setup(x => x.Execute(_emailAddress)).Returns(new User
+                { EmailAddress = _emailAddress, PasswordHash = hashedPassword });
+
+
+            var interactor = new LoginUserInteractor(emailQuery.Object, pwh);
+
+            var result = interactor.Execute(new LoginRequest { EmailAddress = _emailAddress, Password = "AReallyStringPassword" });
+
+
+            Assert.Equal("User Logged In", result.ResponseMessage);
+        }
+    }
+}
---
+++ src/api/Domain/Security/LoginUserInteractor.cs
@@ -0,0 +1,43 @@
+﻿namespace OutreachOperations.Api.Domain.Security
+{
+    public class LoginUserInteractor
+    {
+        private readonly FindUserQueryByEmail _emailQueryObject;
+
+        public LoginUserInteractor(FindUserQueryByEmail emailQueryObject, PasswordHash pwh)
+        {
+            _emailQueryObject = emailQueryObject;
+        }
+
+        public LoginResponse Execute(LoginRequest request)
+        {
+            var user =  _emailQueryObject.Execute(request.EmailAddress);
+
+            if (user == null)
+                 return new LoginResponse{ResponseMessage = "User Not Logged In"};
+                    
+            BCryptPasswordHash pwHash = new BCryptPasswordHash();
+            var result = pwHash.VerifyPassword(request.Password, user.PasswordHash);
+
+            if (result)
+                return new LoginResponse { ResponseMessage = "User Logged In" };
+
+            //TODO:Log login failures
+            //TODO:Log login success
+            //TODO:Generate and return JWT
+
+            return new LoginResponse{ResponseMessage = "User Not Logged In" };
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public string ResponseMessage { get; set; }
+    }
+}
